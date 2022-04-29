0785b1a5e592b4f685f3378d133d8af344b50e08
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-04-29 10:14:51 +0200

    Added User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "John", password: "password")
+User.create(username: "Greta", password: "password")
+User.create(username: "Bard", password: "password")
+User.create(username: "Ola", password: "password")
