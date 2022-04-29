748e9ee2e4ad2f20defeb562edd5f2e3c71f09d3
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-04-29 15:09:43 +0200

    Added flash notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    def new
+    end
+    
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "Logged successfully."
+            redirect_to root_path
+        else  
+            flash.now[:error] = "Your email or password was incorrect."
+            render 'new'
+        end    
+    end    
+
+    def destroy
+        session[:user_id] = nil 
+        flash[:success] = "You were logged out."
+        redirect_to login_path
+    end   
+
 end
